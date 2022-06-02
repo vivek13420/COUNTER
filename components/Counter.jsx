ebb6cd83805461c669af6276de9315073b343f5d
vivek13420 <101565920+vivek13420@users.noreply.github.com>
2022-06-02 10:46:20 +0530

    adsd

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -8,12 +8,23 @@ export default function Counter({ initialValue }) {
   const handleDecrement = () => {
     setCount(count - 1);
   };
+
+  const handleDouble = ()=>{
+    setCount(count*2)
+  }
+  let style
+  if(count%2===0){
+    style="evencounter"
+  }else{
+    style="oddcounter"
+  }
   return (
     <div>
-      <h1>Counter</h1>
-      <h2>{count}</h2>
+      <h1 >Counter</h1>
+      <h2 className={style}>{count}</h2>
       <button onClick={() => handleIncrement()}>Add</button>
       <button onClick={() => handleDecrement()}>Reduce</button>
+      <button onClick={() => handleDouble()}>Double</button>
     </div>
   );
 }
