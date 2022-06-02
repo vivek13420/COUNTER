1d9185374a733f9cd7cfc5f2b37911fa62c9a0c7
vivek13420 <101565920+vivek13420@users.noreply.github.com>
2022-06-02 11:04:59 +0530

    added

--- components/Counter.jsx
+++ components/Counter.jsx
@@ -9,21 +9,21 @@ export default function Counter({ initialValue }) {
     setCount(count - 1);
   };
 
-  const handleDouble = ()=>{
-    setCount(count*2)
-  }
-  let style
-  if(count%2===0){
-    style="evencounter"
-  }else{
-    style="oddcounter"
+  const handleDouble = () => {
+    setCount(count * 2);
+  };
+  let style;
+  if (count % 2 === 0) {
+    style = "evencounter";
+  } else {
+    style = "oddcounter";
   }
   return (
     <div>
-      <h1 >Counter</h1>
+      <h1>Counter</h1>
       <h2 className={style}>{count}</h2>
       <button onClick={() => handleIncrement()}>Add</button>
-      <button onClick={() => handleDecrement()}>Reduce</button>
+      <button onClick={handleDecrement}>Reduce</button>
       <button onClick={() => handleDouble()}>Double</button>
     </div>
   );
